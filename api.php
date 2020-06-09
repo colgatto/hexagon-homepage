38588cadf854d2475fd41f27ac7e56bd6535bf8d
colgatto <colgatto@protonmail.com>
2020-06-09 10:46:50 +0200

    fix get menu

--- api.php
+++ api.php
@@ -5,10 +5,6 @@ header('Content-Type: application/json');
 if(!isset($_POST['action'])) die('');
 
 switch($_POST['action']){
-	case 'get':
-		$json_menu = file_get_contents(__DIR__ . '/menu.json');
-		echo $json_menu;
-		break;
 	case 'set':
 		if(isset($_POST['data']))
 			file_put_contents(__DIR__ . '/menu.json', $_POST['data']);
